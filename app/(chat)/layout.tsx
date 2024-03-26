a89957080d44cdcfab630491f505c76a4f739466
lo_mikeal <mikealnamdy@gmail.com>
2024-03-26 01:16:37 +0100

    ui updates

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,25 +1,25 @@
-import { SidebarDesktop } from '@/components/sidebar-desktop'
-import RightSidebar from '@/components/right-sidebar';
-import '@/app/globals.css';
+import { SidebarDesktop } from "@/components/sidebar-desktop";
+import RightSidebar from "@/components/right-sidebar";
+import "@/app/globals.scss";
 
 interface ChatLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <div className="relative flex h-screen overflow-hidden">
-        <SidebarDesktop />
-        <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:ml-[220px] peer-[[data-state=open]]:xl:ml-[220px]">
-        <article className="md:grid grid-cols-16 gap-1 mx-auto">
-            <main className="col-start-1 col-end-12 relative">{children}</main>
-            <aside className="col-span-5 col-start-12 overflow-y-scroll">
-                <RightSidebar />
-            </aside>
+      <SidebarDesktop />
+      <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:ml-[220px] peer-[[data-state=open]]:xl:ml-[220px]">
+        <article className="lg:grid grid-cols-16 gap-1 mx-auto">
+          <main className="col-start-1 col-end-13 relative">{children}</main>
+          <aside className="col-span-4 col-start-13 overflow-y-scroll">
+            <RightSidebar />
+          </aside>
         </article>
-        </div>
+      </div>
     </div>
   );
 }
 
-export const runtime = 'edge';
+export const runtime = "edge";
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,95 @@
-import type { Metadata } from 'next';
-import { GeistMono } from 'geist/font/mono';
-import { GeistSans } from 'geist/font/sans';
-import { Analytics } from '@vercel/analytics/react';
-import { Toaster } from '@/components/ui/toaster';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "@/components/ui/toaster";
 
-import { AI } from './action';
-import { Providers } from '@/components/providers';
+import { AI } from "./action";
+import { Providers } from "@/components/providers";
 
-import './globals.css';
+import "./globals.scss";
 
 const meta = {
-  title: 'OCADA AI (beta)',
-  description:
-    'Beta version of the OCADA AI Blockchain Agent',
+  title: "OCADA AI (beta)",
+  description: "Beta version of the OCADA AI Blockchain Agent",
 };
 export const metadata: Metadata = {
   ...meta,
   title: {
-    default: 'OCADA AI (beta)',
+    default: "OCADA AI (beta)",
     template: `%s - OCADA AI (beta)`,
   },
   icons: {
-    icon: '/OCADA.svg',
-    shortcut: '/OCADA.svg',
-    apple: '/OCADA.svg',
+    icon: "/OCADA.svg",
+    shortcut: "/OCADA.svg",
+    apple: "/OCADA.svg",
   },
   twitter: {
     ...meta,
-    card: 'summary_large_image',
-    site: '@ocada',
+    card: "summary_large_image",
+    site: "@ocada",
   },
   openGraph: {
     ...meta,
-    locale: 'en-US',
-    type: 'website',
+    locale: "en-US",
+    type: "website",
   },
 };
 
 export const viewport = {
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
 
+const Gabarito = localFont({
+  src: [
+    {
+      path: "../public/fonts/Gabarito-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gabarito-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gabarito-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-gabarito",
+});
+
+const Rethink_Sans = localFont({
+  src: [
+    {
+      path: "../public/fonts/RethinkSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/RethinkSans-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-rethink_sans",
+});
+
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`font-sans antialiased ${GeistSans.variable} ${GeistMono.variable}`}
+        className={`font-sans antialiased ${Gabarito.variable} ${Rethink_Sans.variable} ${GeistSans.variable} ${GeistMono.variable}`}
       >
         <Toaster />
         <AI>
@@ -63,7 +100,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             disableTransitionOnChange
           >
             <main className="flex flex-col flex-1 h-screen bg-[#121212]">
-              {children}  
+              {children}
             </main>
           </Providers>
         </AI>
@@ -73,4 +110,4 @@ export default function RootLayout({ children }: RootLayoutProps) {
   );
 }
 
-export const runtime = 'edge';
+export const runtime = "edge";
