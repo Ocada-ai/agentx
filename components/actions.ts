e30547fae950061f9c4e23e0de7c80c8d48fb391
goodness5 <goodnesskolapo@gmail.com>
2024-12-19 18:54:51 +0100

    merge

---
+++ components/actions.ts
@@ -0,0 +1,16 @@
+export const actions = [
+  {
+    label: 'Jupiter Swap',
+    onClick: () => {
+      console.log('Jupiter Swap action triggered');
+      // Add your Jupiter Swap logic here
+    },
+  },
+  {
+    label: 'Another Action',
+    onClick: () => {
+      console.log('Another action triggered');
+      // Add your other action logic here
+    },
+  },
+];
