c0405b71459d94d2467d465cee62bd0c229d425d
dadsec-dev <geecypher41@gmail.com>
2024-12-19 21:08:46 +0100

    jupiter

--- utils/jupiterUtils.ts
+++ utils/jupiterUtils.ts
@@ -2,7 +2,7 @@ import { Connection, Keypair, VersionedTransaction } from '@solana/web3.js';
 import fetch from 'cross-fetch';
 import { Wallet } from '@project-serum/anchor';
 import bs58 from 'bs58';
-
+  
 
 export async function jupiterSwap() {
     try {
